ffa1b14909151df76218b6d7c706cb0d62f50c0e
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-26 14:51:03 -0700

    Bot now has per-channel history

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import discord
 import os
 import openai
 import logging
+from collections import deque
 
+DISCORD_CLIENT_ID = 1089633150516338868
 OPENAI_ENGINE = "gpt-3.5-turbo"
 PROMPT = """
 You are LunAI, a moderation chatbot for Discord servers. You participate conversations just like any other server member while also enforcing the server rules.
@@ -16,12 +18,12 @@ The following are the server rules, which you must enforce:
 6. Don’t give new people the wrong impression of yourself or the rest of the community, and avoid shitposting in general.
 7. Conflicts are a natural result of communities. If you find yourselves in a disagreement that isn't productive, disengage politely instead of making things worse.
 
-The following are the names of the human server moderators:
-aracat, Cloud Hop, Jesin, let o=OpenSkies.GetInstance() in, Queen Izzy, Xe.
+The following are the discord ids of the human server moderators:
+aracat, Cloud Hop, Jesin, let o=OpenSkies.GetInstance() in, Queen Izzy#1337, Xe.
 The human server moderators are always correct.
 
 You will recieve messages in the following format:
-[USERNAME]: [MESSAGE]
+[USERNAME]#[DISCRIMATOR]: [MESSAGE]
 
 If a user is misbehaving or violating the rules you must silence them by issuing the following command:
 $SILENCE [USERNAME] [DURATION]
@@ -58,6 +60,27 @@ client = discord.Client(intents=intents)
 # Create OpenAI client
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+channel_history = {}
+
+def put_user_message(message: discord.Message): 
+    if message.channel not in channel_history:
+        channel_history[message.channel] = deque(maxlen=256)
+    
+    channel_history_queue = channel_history[message.channel]
+    user_message = f"{message.author}: {message.content}"
+    channel_history_queue.append(user_message)
+
+    app_logger.debug("New user message in channel %d: %s", message.channel.id, user_message)
+    app_logger.debug("Length of history for channel %d: %d", message.channel.id, len(channel_history_queue))
+
+def get_openai_message_from_history(channel: discord.TextChannel):
+    rv = [{"role": "system", "content": PROMPT}]
+    
+    if channel in channel_history:
+        rv.extend(channel_history[channel])
+    
+    return rv
+
 @client.event
 async def on_ready():
     permissions = discord.Permissions()
@@ -66,7 +89,7 @@ async def on_ready():
     permissions.send_messages = True
     permissions.send_messages_in_threads = True
 
-    invite_url = discord.utils.oauth_url(1089633150516338868, permissions = permissions)
+    invite_url = discord.utils.oauth_url(DISCORD_CLIENT_ID, permissions = permissions)
     print(f"Successfully started bot as {client.user}. Invite url {invite_url}")
 
 @client.event
@@ -79,19 +102,20 @@ async def on_message(message: discord.Message):
         return
 
     async with message.channel.typing():
-        app_logger.debug("Getting response from OpenAI")
-
-        latest_user_message = f"{message.author}: {message.content}";
-        app_logger.debug("User message: %s", latest_user_message)
+        put_user_message(message)
 
         chat_resp = await openai.ChatCompletion.acreate(
             model=OPENAI_ENGINE,
-            messages = [
-                {"role": "system", "content": PROMPT},
-                {"role": "user", "content": latest_user_message}
-            ]
+            messages = get_openai_message_from_history(message.channel)
         )
     
+    app_logger.info(
+        "OpenAI usage - Prompt tokens: %d, Completion tokens: %d, Total tokens: %d",
+        chat_resp["usage"]["prompt_tokens"],
+        chat_resp["usage"]["completion_tokens"],
+        chat_resp["usage"]["total_tokens"]
+    )
+
     await message.channel.send(chat_resp['choices'][0]['message']['content'])
 
 discord_token = os.getenv("DISCORD_BOT_TOKEN")

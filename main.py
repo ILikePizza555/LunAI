22427e8b2c170b7f6c82ed829046b4761bcb3c8b
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-26 13:25:41 -0700

    Better logging

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ discord_logger.setLevel(logging.DEBUG)
 # Set the discord http logger level to info
 logging.getLogger("discord.http").setLevel(logging.INFO)
 
+# Create a stream handler, set it's format and add it the root logger
+stderr_handler = logging.StreamHandler()
+stderr_handler.setFormatter(logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}",
+    datefmt = "%Y-%m-%d %H:%M:%S",
+    style = "{"
+))
+logging.getLogger().addHandler(stderr_handler)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -15,7 +24,14 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Successfully started bot as {client.user}")
+    permissions = discord.Permissions()
+    permissions.read_messages = True
+    permissions.moderate_members = True
+    permissions.send_messages = True
+    permissions.send_messages_in_threads = True
+
+    invite_url = discord.utils.oauth_url(1089633150516338868, permissions = permissions)
+    print(f"Successfully started bot as {client.user}. Invite url {invite_url}")
 
 @client.event
 async def on_message(message):

7ee7cfb58dea77bcb5d24a3edec3a49bc2a065a8
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-26 13:42:33 -0700

    Get latest openai python package

--- flake.nix
+++ flake.nix
@@ -10,7 +10,22 @@
         pkgs = import nixpkgs { inherit system; };
         python-packages = p: with p; [
             discordpy
-            openai
+            (buildPythonPackage rec {
+                pname = "openai";
+                version = "0.27.2";
+                src = fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-WGn9+jSw7GbDmvoi9KD7g6E13/gfZQX1KDTGqzET92I=";
+                };
+                doCheck = false;
+                propagatedBuildInputs = [
+                    p.wandb
+                    p.requests
+                    p.tqdm
+                    p.typing-extensions
+                    p.aiohttp
+                ];
+            })
         ];
         python-with-packages = pkgs.python310.withPackages python-packages;
     in

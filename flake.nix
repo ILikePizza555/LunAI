2d9b7b8241ef3ea22a96d37b558060096a275857
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-25 23:59:51 -0700

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
-        system = "aarch64-darwin";
-      };
+        pkgs = import nixpkgs { inherit system; };
     in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.python311
-          ];
-        };
-      };
+        {
+            devShells.default = pkgs.mkShell {
+                buildInputs = [
+                    pkgs.python311
+                ];
+            };
+        }
+    );
 }

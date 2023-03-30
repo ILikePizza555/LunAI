36e3d247b2a76f0c1416e3956600bdc382ce4b2c
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-29 18:56:23 -0700

    Remove memoization for now as it breaks things

--- ai.py
+++ ai.py
@@ -1,6 +1,5 @@
 from enum import Enum
 from dataclasses import dataclass, field
-from functools import lru_cache
 from tiktoken import get_encoding, Encoding
 from typing import Union
 import heapq
@@ -10,9 +9,7 @@ class MessageRole(Enum):
     USER = "user"
     ASSISTANT = "assistant"
 
-# dataclass will only generate hash if both eq and frozen are true
-# Message is "logically" frozen. So we set unsafe_hash here to force the generation of __hash__()
-@dataclass(order=True, unsafe_hash=True)
+@dataclass(order=True)
 class Message:
     """Dataclass to store metadata and content for messages between us and the chat completion model"""
     # The priority of the message
@@ -30,8 +27,7 @@ class Message:
             "role": self.role.value,
             "content": self.content
         }
-    
-@lru_cache
+
 def calculate_message_tokens(message: Message, encoding: Union[str, Encoding] = "cl100k_base") -> int:
     if type(encoding) is str:
         encoding = get_encoding(encoding)

ad39d10e1cba5050e1ff7bde7d92c412eb667730
Izzy Lancaster <avrisaac555@gmail.com>
2023-03-30 03:05:18 -0700

    Refactor calculate tokens and make role and content part of Message hash

--- ai.py
+++ ai.py
@@ -1,18 +1,15 @@
 from enum import Enum
 from collections import deque
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 from functools import lru_cache
 from tiktoken import get_encoding, Encoding
-from typing import Union
 
 class MessageRole(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
 
-# dataclass will only generate hash if both eq and frozen are true
-# Message is "logically" frozen. So we set unsafe_hash here to force the generation of __hash__()
-@dataclass(order=True, unsafe_hash=True)
+@dataclass(order=True, frozen=True)
 class Message:
     """Dataclass to store metadata and content for messages between us and the chat completion model"""
     # The priority of the message
@@ -20,9 +17,9 @@ class Message:
     # The "index" of the message. Newer messages have higher numbers. Should be an incrementing integer.
     index: int
     # The role of the message. Gets sent to the API
-    role: MessageRole = field(compare=False)
+    role: MessageRole = field(compare=False, hash=True)
     # The message itself. Gets sent to the API
-    content: str = field(compare=False)
+    content: str = field(compare=False, hash=True)
 
     def api_serialize(self):
         """Converts this object into a dict that can be passed to the OpenAI API."""
@@ -31,18 +28,19 @@ class Message:
             "content": self.content
         }
     
-@lru_cache
-def calculate_message_tokens(message: Message, encoding: Union[str, Encoding] = "cl100k_base") -> int:
-    if type(encoding) is str:
-        encoding = get_encoding(encoding)
-    
-    return len(encoding.encode(message.content))
+    @staticmethod
+    @lru_cache
+    def calculate_tokens(message, encoding: str | Encoding = "cl100k_base") -> int:
+        if type(encoding) is str:
+            encoding = get_encoding(encoding)
+        
+        return len(encoding.encode(message.content))
 
 
 class ModelContextWindow:
     """Manages messages to form a context window that can be passed to the model."""
 
-    def __init__(self, max_tokens: int, encoding: Union[str, Encoding] = "cl100k_base"):
+    def __init__(self, max_tokens: int, encoding: str | Encoding = "cl100k_base"):
         self._queue = deque()
         self._token_count = 0
         self.encoding = encoding
@@ -63,7 +61,7 @@ class ModelContextWindow:
 
         while self._token_count > self.max_tokens:
             m = self._queue.popleft()
-            m_tokens = calculate_message_tokens(m, self.encoding)
+            m_tokens = Message.calculate_tokens(m, self.encoding)
             
             self._token_count -= m_tokens
             rv.append(m_tokens)
@@ -72,7 +70,7 @@ class ModelContextWindow:
     
     def insert_message(self, message: Message) -> list[Message]:
         """Inserts a message into the window. Automatically calls drain_tokens"""
-        token_count = calculate_message_tokens(message, self.encoding)
+        token_count = Message.calculate_tokens(message, self.encoding)
         self._token_count += token_count
 
         self._queue.append(message)
